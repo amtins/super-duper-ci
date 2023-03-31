4db810bb54f0d64b62372431ffd020c58e020e8c
amtins <andre.martins@rts.ch>
2023-03-31 16:32:34 +0200

    feat(app): add custom button

--- src/app.js
+++ src/app.js
@@ -1,15 +1,18 @@
 import videojs from 'video.js';
 import getAppVersion from './version.js';
 
-document.querySelector('.version').textContent = getAppVersion();
+import './components/custom-button.js';
 
+window.videojs = videojs;
 window.player = videojs('player', {
   controls: true,
+  customButton: true,
   muted: true,
   sources: [
     'https://d2zihajmogu5jn.cloudfront.net/bipbop-advanced/bipbop_16x9_variant.m3u8'
   ],
   width: '640px',
   height: '360px',
-  autoplay: 'muted',
 });
+
+document.querySelector('.version').textContent = getAppVersion();
