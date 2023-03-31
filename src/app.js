172e4d0de249eb67d372fede5e298d3b8e8554c1
amtins <andre.martins@rts.ch>
2023-03-31 10:57:30 +0200

    feat(player): add basic player experience

--- src/app.js
+++ src/app.js
@@ -1,3 +1,13 @@
+import videojs from 'video.js';
 import getAppVersion from './version.js';
 
 document.querySelector('.version').textContent = getAppVersion();
+
+window.player = videojs('player', {
+  controls: true,
+  muted: true,
+  sources:['https://d2zihajmogu5jn.cloudfront.net/bipbop-advanced/bipbop_16x9_variant.m3u8'],
+  width: '640px',
+  height: '360px',
+});
+
