ace2711c3a661a11eca3ddd4aad674031e5b108f
amtins <andre.martins@rts.ch>
2023-03-31 16:31:35 +0200

    feat(custom-button): add new button

---
+++ src/components/custom-button.js
@@ -0,0 +1,34 @@
+/**
+ * @file custom-button.js
+ */
+import videojs from 'video.js';
+import { version } from '../../package.json';
+
+/**
+ * Base class for all buttons.
+ *
+ * @extends Button
+ */
+class CustomButton extends videojs.getComponent('Button') {
+  handleClick() {
+    // eslint-disable-next-line
+    console.log({
+      appVersion: version,
+      videojs: videojs.VERSION,
+      vhs: videojs.VhsHandler.version(),
+    });
+  }
+
+  /**
+   * Build css class.
+   *
+   * @returns {String}
+   */
+  buildCSSClass() {
+    return`super-custom-button ${super.buildCSSClass()}`.trim();
+  }
+}
+
+videojs.registerComponent(CustomButton.name, CustomButton);
+
+export default CustomButton;
